f02ebc236d21ef233b05718006e78a7b7f0e7f1e
Hassan220022 <hassansonson2002@gmail.com>
2024-11-05 02:28:23 +0200

    added all the changes and all the features

--- lib/screens/surah_reader.dart
+++ lib/screens/surah_reader.dart
@@ -1,5 +1,4 @@
 // lib/screens/surah_reader.dart
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
@@ -7,13 +6,12 @@ import 'dart:convert';
 import '../utils/provider/preference_settings_provider.dart';
 import '../utils/provider/bookmarks_provider.dart';
 import '../models/bookmark.dart';
+import '../services/audio_player_service.dart'; // Updated import
 
 class Ayah {
   final int numberInSurah;
   final String text;
-
   Ayah({required this.numberInSurah, required this.text});
-
   factory Ayah.fromJson(Map<String, dynamic> json) {
     return Ayah(
       numberInSurah: json['numberInSurah'],
@@ -26,14 +24,12 @@ class SurahReaderScreen extends StatefulWidget {
   final int surahNumber;
   final String surahName;
   final int? highlightAyah; // Optional parameter for highlighting
-
   const SurahReaderScreen({
     Key? key,
     required this.surahNumber,
     required this.surahName,
     this.highlightAyah,
   }) : super(key: key);
-
   @override
   _SurahReaderScreenState createState() => _SurahReaderScreenState();
 }
@@ -42,12 +38,14 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
   List<Ayah> _ayahs = [];
   bool _isLoading = true;
   bool _isError = false;
-
   static const String basmallahImagePath = 'assets/basmallah.png';
+  late AudioPlayerService _audioPlayerService; // Initialize AudioPlayerService
+  int? _currentlyPlayingAyah;
 
   @override
   void initState() {
     super.initState();
+    _audioPlayerService = AudioPlayerService();
     fetchAyahs();
   }
 
@@ -59,7 +57,6 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
         List<Ayah> fetchedAyahs = [];
-
         if (data['status'] == 'OK') {
           final List<dynamic> ayahs = data['data']['ayahs'];
           for (var ayah in ayahs) {
@@ -70,7 +67,6 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
             }
           }
         }
-
         setState(() {
           _ayahs = fetchedAyahs;
           _isLoading = false;
@@ -89,13 +85,43 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
     }
   }
 
-  // Utility function to normalize text
   String normalizeText(String input) {
     final diacritics = RegExp(r'[\u064B-\u0652]');
     return input
         .replaceAll(diacritics, '')
         .replaceAll(RegExp(r'\s+'), ' ')
         .trim();
+  }
+
+  void _playAudio(int ayahNumber) async {
+    String surahStr = widget.surahNumber.toString().padLeft(3, '0');
+    String ayahStr = ayahNumber.toString().padLeft(3, '0');
+    String audioUrl =
+        'https://everyayah.com/data/AbdulSamad_64kbps_QuranExplorer.Com/$surahStr$ayahStr.mp3';
+
+    try {
+      await _audioPlayerService.play(audioUrl);
+      setState(() {
+        _currentlyPlayingAyah = ayahNumber;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error playing audio')),
+      );
+    }
+  }
+
+  void _stopAudio() async {
+    try {
+      await _audioPlayerService.stop();
+      setState(() {
+        _currentlyPlayingAyah = null;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error stopping audio')),
+      );
+    }
   }
 
   void _showBookmarkDialog(Ayah ayah) {
@@ -138,14 +164,15 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
   }
 
   @override
+  void dispose() {
+    _audioPlayerService.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     bool isDarkTheme =
         Provider.of<PreferenceSettingsProvider>(context).isDarkTheme;
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
-    EdgeInsets safePadding = MediaQuery.of(context).padding;
-    double horizontalPadding = 16.0;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -181,14 +208,12 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
                       color: isDarkTheme ? Colors.white : Colors.black,
                       fontSize: 16.0,
                     ),
-                    textAlign: TextAlign.center,
                   ),
                 )
               : ListView.builder(
-                  itemCount: _ayahs.length + 1, // +1 for the Basmallah png
+                  itemCount: _ayahs.length + 1, // +1 for Basmallah
                   itemBuilder: (context, index) {
                     if (index == 0) {
-                      // First item is the Basmallah PNG image
                       return Padding(
                         padding: const EdgeInsets.all(16.0),
                         child: ColorFiltered(
@@ -225,17 +250,14 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
                         ),
                       );
                     }
-
                     final ayah = _ayahs[index - 1];
                     bool isHighlighted = widget.highlightAyah != null &&
                         ayah.numberInSurah == widget.highlightAyah;
-
+                    bool isPlaying =
+                        _currentlyPlayingAyah == ayah.numberInSurah;
                     return Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: horizontalPadding +
-                            (isLandscape
-                                ? safePadding.left + safePadding.right
-                                : 0.0),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
                         vertical: 8.0,
                       ),
                       child: GestureDetector(
@@ -271,16 +293,40 @@ class _SurahReaderScreenState extends State<SurahReaderScreen> {
                                   ),
                                 ),
                               ),
+                              // IconButton(
+                              //   icon: Icon(
+                              //     isPlaying ? Icons.stop : Icons.play_arrow,
+                              //     color: isDarkTheme
+                              //         ? Colors.white
+                              //         : const Color(0xff682DBD),
+                              //   ),
+                              //   onPressed: () {
+                              //     if (isPlaying) {
+                              //       _stopAudio();
+                              //     } else {
+                              //       _playAudio(ayah.numberInSurah);
+                              //     }
+                              //   },
+                              // ),
                               const SizedBox(width: 12.0),
                               // Ayah Text
                               Expanded(
-                                child: Text(
-                                  ayah.text,
-                                  style: const TextStyle(
-                                    fontFamily: 'Quran',
-                                    fontSize: 20.0,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    if (isPlaying) {
+                                      _stopAudio();
+                                    } else {
+                                      _playAudio(ayah.numberInSurah);
+                                    }
+                                  },
+                                  child: Text(
+                                    ayah.text,
+                                    style: const TextStyle(
+                                      fontFamily: 'Quran',
+                                      fontSize: 20.0,
+                                    ),
+                                    textDirection: TextDirection.rtl,
                                   ),
-                                  textDirection: TextDirection.rtl,
                                 ),
                               ),
                             ],
